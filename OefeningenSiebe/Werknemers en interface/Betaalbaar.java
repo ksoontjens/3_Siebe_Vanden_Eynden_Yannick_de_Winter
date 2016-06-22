7fdece9bd140adcad8fd3a3067d4c619764fc177
Siebe Vanden Eynden <siebe.vandeneynden@student.kdg.be>
2016-06-22 17:38:10 +0200

    Juiste oefeingen

--- OefeningenSiebe/Werknemers en interface/Betaalbaar.java
+++
@@ -1,4 +0,0 @@
-public interface Betaalbaar
-{
-	abstract public void betaal();
-}
--- OefeningenSiebe/Werknemers en interface/StudentWerknemer.java
+++
@@ -1,8 +0,0 @@
-public class StudentWerknemer extends PartTimeWerknemer
-{
-	public StudentWerknemer(String voornaam, String achternaam, int wNummer, float salaris, int urenGw)
-	{
-		super(voornaam, achternaam, wNummer, salaris, urenGw);
-		setRSZ(5.0f);
-	}
-}
