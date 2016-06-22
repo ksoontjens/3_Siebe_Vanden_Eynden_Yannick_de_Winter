1feedebe18b2a5733125003ff83204c6580ec473
YannickDW <yannick-de-winter@telenet.be>
2016-06-22 12:35:17 +0200

    Oefeningen Yannick De Winter

---
+++ NetBeansProjects/OefeningenImages_1/src/hellotvxlet/ButtonComponent.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+package hellotvxlet;
+ 
+import java.awt.Color;
+import java.awt.Font;
+import org.havi.ui.HComponent;
+import org.havi.ui.HScene;
+import org.havi.ui.HTextButton;
+import org.havi.ui.HVisible;
+ 
+/**
+ *
+ * @author student
+ */
+public class ButtonComponent extends HComponent{
+   
+    private HTextButton button;
+    private Color color;
+   
+    private int grootte = 60;
+ 
+    public ButtonComponent(Color color, int x, int y, String name, HScene scene)
+    {
+        this.button=new HTextButton(name, x, y, grootte, grootte);
+        this.button.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        this.button.setBackground(color);
+        this.color = color;
+        this.button.setBordersEnabled(false);
+       
+        scene.add(button);
+    }
+    //public ButtonComponent(int x, HScene scene)
+    //{
+    //    this.button=new HTextButton("", x, 160, 60, 60);
+    //    this.button.setBackgroundMode(HVisible.BACKGROUND_FILL);
+    //    this.button.setBackground(Color.WHITE);
+    //    this.color = Color.WHITE;
+    //    this.button.setBordersEnabled(false);
+       
+    //    scene.add(button);
+    //}
+   
+    public ButtonComponent(int size, int x, int y, HScene scene)
+    {
+        this.button=new HTextButton("", x, y, size, size);
+        this.button.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        this.button.setBackground(Color.WHITE);
+        this.color = Color.WHITE;
+        this.button.setBordersEnabled(false);
+       
+        scene.add(button);
+    }
+           
+ 
+    public ButtonComponent(Color color, int x, int size, HScene scene)
+    {
+        this.button=new HTextButton("", x, 5, size, size);
+        this.button.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        this.button.setBackground(color);
+        this.button.setBordersEnabled(false);
+        this.color = color;
+       
+        scene.add(button);
+    }
+   
+    public HTextButton getButton()
+    {
+        return this.button;
+    }
+   
+    public Color getColor()
+    {
+        return this.color;
+    }
+   
+    public void changeColor (Color color, HScene scene)
+    {
+        this.button.setBackground(color);
+        scene.repaint();
+        this.color = color;
+    }
+   
+    public void changeX(int x, HScene scene)
+    {
+        this.button.setLocation(x, 155);
+        scene.repaint();
+    }
+ 
+    public void changePos(int x, int y, HScene scene) {
+        this.button.setLocation(x, y);
+        scene.repaint();
+    }
+   
+    public boolean isWhite()
+    {
+        return this.color == Color.WHITE;
+    }
+}
